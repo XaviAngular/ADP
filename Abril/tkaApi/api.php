72051b3b3c3d4b776d62bad3d22e5b67e737efaf
XaviAngular <xavcet@gmail.com>
2017-04-05 18:02:33 +0200

    añadir getplato para devolver detalle de un plato

--- Abril/tkaApi/api.php
+++ Abril/tkaApi/api.php
@@ -16,6 +16,15 @@ $app->get("/getPlatos", function() use($db, $app) {
 	echo json_encode([
 		"status" 	=> "success",
 		"data"		=> $platos
+		]);		
+});
+
+$app->get("/getPlato/:id", function($id) use($db, $app) {
+	$query = $db->query("SELECT * FROM platos WHERE id=$id");
+	$plato = $query->fetch_assoc();
+	echo json_encode([
+		"status" 	=> "success",
+		"data"		=> $plato
 		]);	
 	
 });
